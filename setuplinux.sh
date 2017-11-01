b86dd60535c4af2247424c26cb94e6e0eebec8d1
Gustavo Eyken <38012@alunos.bandtec.com.br>
2017-10-31 22:24:44 -0200

    Arquivos de Redes

---
+++ setuplinux.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+apt-get update
+apt-get install apache2 php5 mysql vim xss csrlf kkkk
+apt-get install pacote1
+echo "8.8.8.8" >> /etc/resolv.conf
+echo "4.4.4.4" >> /etc/resolv.conf
+cmd1
+cmd2
+cmd3
